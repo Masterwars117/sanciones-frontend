7d704ef7f1c20167d4cc7414356bc79cc8fb33c1
Carlos Emmanuel Rivera Pérez <charlyerp240@gmail.com>
2026-05-28 14:15:50 -0600

    Enhance nuxt.config.ts with runtime configuration for API and OAuth settings; update package.json and package-lock.json to include @types/node; adjust tsconfig.json path for node configuration.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite"
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   
@@ -16,15 +17,18 @@ export default defineNuxtConfig({
   imports: {
     dirs: ["services"],
   },
-  nitro: {
-    devProxy: {
-      "/api": {
-        //localhost
-        target: "http://127.0.0.1:8000/api",
-        //server
-        //target: "http://10.15.10.38:8000/api",
-        changeOrigin: true,
-      },
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.NUXT_API_URL || '',
+      oauthClientId: process.env.NUXT_OAUTH_CLIENT_ID || '',
+      oauthRedirectUri: process.env.VITE_OAUTH_REDIRECT_URI || '',
+      oauthBaseUrl: process.env.VITE_OAUTH_BASE_URL || '',
+      oauthAuthUrl: process.env.VITE_OAUTH_AUTH_URL || '',
+      oauthTokenUrl: process.env.VITE_OAUTH_TOKEN_URL || '',
+      oauthRevokeUrl: process.env.VITE_OAUTH_REVOKE_URL || '',
+      oauthScope: process.env.VITE_OAUTH_SCOPE || '',
+      appName: process.env.VITE_APP_NAME || 'Gestión Documental | SABG',
+      appVersion: process.env.VITE_APP_VERSION || '1.0.0',
     },
   },
 })
